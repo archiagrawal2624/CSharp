587269aef696de363188301a645bae317f071611
archiagrawal2624 <agrawalarchi8058@gmail.com>
2024-09-14 15:41:46 +0530

    Readme

---
+++ Readme.md
@@ -0,0 +1,1 @@
+<h4><CSharp--Project/h4>
--- obj/Debug/net8.0/ConsoleApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5626ec61444a8bbfc330b8dc776495400e3b942")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
